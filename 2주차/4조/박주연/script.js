034861c24343ffac7d2ca1fc6a8202714d65014c
jjuya <jjuya9381@naver.com>
2018-07-23 16:38:20 +0900

    Firebase_Signup, Login

--- 2주차/4조/박주연/script.js
+++ 2주차/4조/박주연/script.js
@@ -1,11 +1,38 @@
 function signup() {
+  firebase.auth().createUserWithEmailAndPassword(getEmail(), getPassword())
+    .then(
+      function(success) {
+        console.log(success);
+        alert("회원가입 성공");
+      }, function(error) {
+        console.log(error);
+        alert("회원가입 실패");
+      }
+    );
   console.log("회원가입");
 }
 
+$("#signup-btn").click(
+  function() {
+    signup();
+  }
+);
+
 function login() {
-  console.log("로그인");
-  $("#login-frame").hide();
-  $("#db-upload-frame").show();
+  firebase.auth().signInWithEmailAndPassword(getEmail(), getPassword())
+  .then(
+    function(success) {
+      console.log(success);
+      alert("로그인 성공");
+      $("#login-frame").hide();
+      $("#db-upload-frame").show();
+    }, function(error) {
+      console.log(error);
+      alert("로그인 실패");
+    }
+  );
+
+  console.log("로그인 함수 실행 끝");
 }
 
 function logout() {
@@ -26,3 +53,15 @@ $("#logout-btn").click(
     logout();
   }
 );
+
+function getEmail() {
+  return $("#email").val();
+}
+
+function getPassword() {
+  return $("#password").val();
+}
+
+function setDBdata(data) {
+  $("#real-time-val").text(data);
+}
