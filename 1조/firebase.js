46e154fcbd5a9d4d69b7008323a789c37d0703d7
jjuguriKing <kyi108@naver.com>
2018-08-05 18:12:12 +0900

    3주차 과제 업로드

---
+++ 1조/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBwzWp3mBi7qs5lZcVfsKM_sx8rcONApf4",
+    authDomain: "dodream-developer-1.firebaseapp.com",
+    databaseURL: "https://dodream-developer-1.firebaseio.com",
+    projectId: "dodream-developer-1",
+    storageBucket: "dodream-developer-1.appspot.com",
+    messagingSenderId: "678421291093"
+  };
+  firebase.initializeApp(config);
